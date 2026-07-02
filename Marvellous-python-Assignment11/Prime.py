7a2d886cee49d2f104f76db85bd6fd9b8620531f
Aniket Jadhav <aniket.jadhav.080802@gmail.com>
2026-07-02 13:56:23 +0530

    Add prime number checking function

---
+++ Marvellous-python-Assignment11/Prime.py
@@ -0,0 +1,13 @@
+def CheckPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input("Enter a number: "))
+if CheckPrime(n):
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
