2794ba2c959b7ad368be74d757b3f78f1d730405
Aniket Jadhav <aniket.jadhav.080802@gmail.com>
2026-07-11 23:54:47 +0530

    Add lambda function to calculate square of a number

---
+++ Marvellous-python-Assignment19/LambdaOne.py
@@ -0,0 +1,8 @@
+def Main():
+    PowerFun = lambda x: x ** 2
+    no = int(input("Enter number : "))
+    ret = PowerFun(no)
+    print("Power of two is : ", ret)
+
+if __name__ == "__main__":
+    Main()
